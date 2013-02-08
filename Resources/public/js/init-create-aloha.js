3fbefbeefd87970ecbb0dc6a4a6112af2c180940
Simon Constans <simonc@theodo.fr>
2013-02-08 19:09:51 +0100

    add aloha configuration

--- Resources/public/js/init-create-aloha.js
+++ Resources/public/js/init-create-aloha.js
@@ -1,30 +1,29 @@
 jQuery(document).ready(function() {
-  // Instantiate Create
-  jQuery('body').midgardCreate({
-    url: function() {
-      return 'javascript:false;';
-    },
-    // Use Aloha for all text editing
-    editorWidgets: {
-      'default': 'aloha'
-    },
-    editorOptions: {
-      aloha: {
-        widget: 'alohaWidget'
-      }
-    },
-    collectionWidgets: {
-      'default': null,
-      'feature': 'midgardCollectionAdd'
-    },
-    stanbolUrl: 'http://dev.iks-project.eu:8081' 
-  });
-  
-  // Fake Backbone.sync since there is no server to communicate with
-  Backbone.sync = function(method, model, options) {
-    if (console && console.log) {
-      console.log('Model contents', model.toJSONLD());
-    }
-    options.success(model);
-  };
+    // Instantiate Create
+    jQuery('body').midgardCreate({
+        url: function() {
+            if (this.id) {
+                if (this.id.charAt(0) == "<") {
+                    return cmfCreatePutDocument + this.id.substring(1, this.id.length - 1);
+                }
+                return cmfCreatePutDocument + "/" + this.id;
+            }
+            return cmfCreatePutDocument;
+        },
+        stanbolUrl: cmfCreateStanbolUrl,
+        tags: true,
+        // Use Aloha for all text editing
+        editorWidgets: {
+            'default': 'aloha'
+        },
+        editorOptions: {
+            aloha: {
+                widget: 'alohaWidget'
+            }
+        },
+        collectionWidgets: {
+            'default': null,
+            'feature': 'midgardCollectionAdd'
+        }
+    });
 });
